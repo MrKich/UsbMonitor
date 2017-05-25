4e52f0e29507bef3756a6292ec77cfba8d445c0a
Alexander <Alex3296@yandex.ru>
2017-05-26 02:02:58 +0300

    Fixed invalid encodings in comments

--- UsbMonitor/DbBindings.cs
+++ UsbMonitor/DbBindings.cs
@@ -18,7 +18,7 @@ namespace UsbMonitor {
         REMOVED,
     }
 
-    // ������� ����� ���� (��������� ��� ��� ����������� ������)
+    // Базовый класс логов (остальные будут от него наследоваться)
     public class LogEntry {
         public int Id { get; set; }
         public string Username { get; set; }
@@ -26,20 +26,20 @@ namespace UsbMonitor {
         public string SerialNumber { get; set; }
     }
 
-    // �����-�������� ���� ��� �������/�������� ������
+    // Класс-описание для логгирования вставки/удаления usb
     public class UsbStateEntry : LogEntry {
         public USB_STATE State { get; set; }
         public string DriveLetter { get; set; }
     }
 
-    // �����-�������� ���� ��� �������� ��� �������
+    // Класс-описание для логгирования операций над файлами
     public class FSWatcherEntry : LogEntry {
         public FILE_STATE State { get; set; }
         public string OldPath { get; set; }
         public string Path { get; set; }
     }
 
-    // �����-�������� ������������������ �������������
+    // Класс-описание зарегистрированных пользователей
     public class RegisteredUser {
         public int Id { get; set; }
         public string Username { get; set; }
@@ -51,31 +51,31 @@ namespace UsbMonitor {
         }
     }
 
-    // �������� ���������� usb
+    // Класс-описание допустимых usb
     public class RegisteredUsbEntry {
         public int Id { get; set; }
         public RegisteredUser User { get; set; }
         public string UsbSerial { get; set; }
     }
 
-    // �������� ����� ��� ���� ������ (��������� ��������� ���� ��������)
+    // Основной класс, который представляет будущую базу данных
     public class LogDatabase : DbContext {
         public LogDatabase() : base(new SQLiteConnection() { ConnectionString =
             new SQLiteConnectionStringBuilder() {
-                // ���������� �������, ������� �������� ������� �����, ������ ��������� ����������
-                // � ��������� ��� ���� � �����
+                // Магическая строчка, которая получает текущую директорию, в которой был запущен наш процесс
+                // и добавляет имя базы в конце
                 DataSource = Path.Combine(Path.GetDirectoryName(
                     System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase.Substring(8)), "UsbMonitor.db"),
         ForeignKeys = true }.ConnectionString}, true) { }
 
-        // Entity Framework �� ������������ �������� ��� ������ � ����
-        // ������� ��� � ���� � ������� ���������� SQLite.CodeFirst
+        // Entity Framework не позволяет создавать базы данных с нуля (если файла на диске нет)
+        // Поможем ему с этим с помощью библиотеки SQLite.CodeFirst
         protected override void OnModelCreating(DbModelBuilder modelBuilder) {
             var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<LogDatabase>(modelBuilder);
             Database.SetInitializer(sqliteConnectionInitializer);
         }
 
-        // ������� ������� � ���� ������
+        // Будущие таблицы в базе данных
         public DbSet<LogEntry> LogEntries { get; set; }
         public DbSet<RegisteredUsbEntry> RegisteredUsbs { get; set; }
         public DbSet<RegisteredUser> RegisteredUsers { get; set; }
