550a9d8c8f73503dbf35118f13ff1071bf61d1f3
Alexander <Alex3296@yandex.ru>
2017-05-26 01:51:30 +0300

    Added comments to dbbindings

--- UsbMonitor/DbBindings.cs
+++ UsbMonitor/DbBindings.cs
@@ -18,6 +18,7 @@ namespace UsbMonitor {
         REMOVED,
     }
 
+    // ������� ����� ���� (��������� ��� ��� ����������� ������)
     public class LogEntry {
         public int Id { get; set; }
         public string Username { get; set; }
@@ -25,17 +26,20 @@ namespace UsbMonitor {
         public string SerialNumber { get; set; }
     }
 
+    // �����-�������� ���� ��� �������/�������� ������
     public class UsbStateEntry : LogEntry {
         public USB_STATE State { get; set; }
         public string DriveLetter { get; set; }
     }
 
+    // �����-�������� ���� ��� �������� ��� �������
     public class FSWatcherEntry : LogEntry {
         public FILE_STATE State { get; set; }
         public string OldPath { get; set; }
         public string Path { get; set; }
     }
 
+    // �����-�������� ������������������ �������������
     public class RegisteredUser {
         public int Id { get; set; }
         public string Username { get; set; }
@@ -47,25 +51,31 @@ namespace UsbMonitor {
         }
     }
 
+    // �������� ���������� usb
     public class RegisteredUsbEntry {
         public int Id { get; set; }
         public RegisteredUser User { get; set; }
         public string UsbSerial { get; set; }
     }
 
+    // �������� ����� ��� ���� ������ (��������� ��������� ���� ��������)
     public class LogDatabase : DbContext {
         public LogDatabase() : base(new SQLiteConnection() { ConnectionString =
             new SQLiteConnectionStringBuilder() {
-                //DataSource = @"D:\disk\Projects\UsbMonitor\UsbMonitor.db",
+                // ���������� �������, ������� �������� ������� �����, ������ ��������� ����������
+                // � ��������� ��� ���� � �����
                 DataSource = Path.Combine(Path.GetDirectoryName(
                     System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase.Substring(8)), "UsbMonitor.db"),
         ForeignKeys = true }.ConnectionString}, true) { }
 
+        // Entity Framework �� ������������ �������� ��� ������ � ����
+        // ������� ��� � ���� � ������� ���������� SQLite.CodeFirst
         protected override void OnModelCreating(DbModelBuilder modelBuilder) {
             var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<LogDatabase>(modelBuilder);
             Database.SetInitializer(sqliteConnectionInitializer);
         }
 
+        // ������� ������� � ���� ������
         public DbSet<LogEntry> LogEntries { get; set; }
         public DbSet<RegisteredUsbEntry> RegisteredUsbs { get; set; }
         public DbSet<RegisteredUser> RegisteredUsers { get; set; }
